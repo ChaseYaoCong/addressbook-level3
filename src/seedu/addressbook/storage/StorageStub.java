3a405d73ec97ccd074679c644a5ac6b5e76f5e0b
chasexyaocong <tiongyaocong@gmail.com>
2016-09-24 14:58:40 +0800

    Implement Storage Class

---
+++ src/seedu/addressbook/storage/StorageStub.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+
+public class StorageStub extends Storage {
+
+    public StorageStub(String storageFilePath) {
+    }
+    
+    @Override
+    public AddressBook load() throws StorageOperationException {
+        return new AddressBook();
+    }
+
+    @Override
+    public void save(AddressBook book) throws StorageOperationException {
+    }
+
+    @Override
+    public String getPath() {
+        return null;
+    }
+    
+
+}
